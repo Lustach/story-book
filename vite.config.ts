3d648963c9e5dc2b46e08727a04f2475a12f76b3
Lustach <lusta.vlad2001@gmail.com>
2023-04-26 19:41:19 +0300

    Add: docker

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8081,
+    host: '0.0.0.0'
+  },
   plugins: [
     vue(),
     vueJsx(),
